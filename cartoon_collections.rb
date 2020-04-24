5567d6fbc93b1c21a48c9bfa4020fdb7be4e7b2a
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-24 05:47:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index { |num, name| puts "#{num}. #{name}"} 
+  dwarves.each_with_index { |num, name| puts "#{num + 1}. #{name}"} 
 end
 
 def summon_captain_planet# code an argument here
